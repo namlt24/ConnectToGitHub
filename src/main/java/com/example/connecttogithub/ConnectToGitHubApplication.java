dcf58c9fb37b442a97b7a69554dd34837a030041
namlt24 <dolechtieuchuan4@gmail.com>
2023-07-22 08:03:35 +0700

    namlt24 commit first connect

---
+++ src/main/java/com/example/connecttogithub/ConnectToGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.connecttogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectToGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectToGitHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connecttogithub/ConnectToGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connecttogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectToGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
