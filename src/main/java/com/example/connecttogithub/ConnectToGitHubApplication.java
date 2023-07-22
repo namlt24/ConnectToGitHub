f8ee9e352cd864abdc5c3152cd0540179ba7eb8f
namlt24 <dolechtieuchuan4@gmail.com>
2023-07-22 08:21:52 +0700

    namlt24 commit method

--- src/main/java/com/example/connecttogithub/ConnectToGitHubApplication.java
+++ src/main/java/com/example/connecttogithub/ConnectToGitHubApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConnectToGitHubApplication {
-
+    public static void addText(){
+        System.out.println("Adding text");
+    }
     public static void main(String[] args) {
         SpringApplication.run(ConnectToGitHubApplication.class, args);
     }
